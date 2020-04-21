0570d566bc6694460f07999c00e0fdc45aa78b03
yterletskyi <v-yterletskyi@vrbo.com>
2020-04-21 19:46:47 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+var myArray =["qweqwe", "qweqwe"];
+myArray [0] = 'Привіт';
+myArray [1] = 'Світ';
+console.log(myArray[1], myArray[2]);
+
+let user = {
+    name: myArraya
+    age: ...,
+    status: {
+        text: ...
+    }
+}
+
+user.status.text 
